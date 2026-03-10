1aef186dc5c7bd984a8e02312dbdc5ca2a1595c4
uccs <1500846601@qq.com>
2026-03-10 17:25:23 +0800

    对接邮箱

---
+++ common/response_message.py
@@ -0,0 +1,13 @@
+class UserMessage:
+
+    @staticmethod
+    def success(data):
+        return {"status": 1000, "message": "success", "data": data}
+
+    @staticmethod
+    def error(data):
+        return {"status": 1002, "message": "error", "data": data}
+
+    @staticmethod
+    def other(data):
+        return {"status": 1001, "message": "other", "data": data}
--- controller/user.py
+++ controller/user.py
@@ -1,4 +1,8 @@
-from flask import Blueprint, make_response, session
+import re
+
+from flask import Blueprint, make_response, session, request
+from common import response_message
+from common.email_utils import gen_email_code, send_email
 from common.utils import ImageCode
 from model.user import User
 
@@ -21,3 +25,20 @@ def vcode():
     response.headers["Content-Type"] = "image/jpeg"
     session["vcode"] = code.lower()
     return response
+
+
+@user.route("/ecode", methods=["post"])
+def email_code():
+    email = request.form.get("email")
+    if email is None:
+        return response_message.UserMessage.error("请输入邮箱地址")
+    if not re.match(r".+@.+\..+", email):
+        return response_message.UserMessage.error("请输入正确的邮箱地址")
+    code = gen_email_code()
+    try:
+        send_email(email, code)
+        session["ecode"] = code.lower()
+        return response_message.UserMessage.success("验证码已发送")
+    except Exception as e:
+        print(e)
+        return response_message.UserMessage.error("验证码发送失败")
