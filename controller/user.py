62f7874589fa0663471044a67c7851f047cb5140
uccs <1500846601@qq.com>
2026-03-10 18:10:48 +0800

    注册

--- controller/user.py
+++ controller/user.py
@@ -1,3 +1,5 @@
+import hashlib
+import json
 import re
 
 from flask import Blueprint, make_response, session, request
@@ -29,16 +31,48 @@ def vcode():
 
 @user.route("/ecode", methods=["post"])
 def email_code():
-    email = request.form.get("email")
+    email = json.loads(request.data).get("email")
     if email is None:
         return response_message.UserMessage.error("请输入邮箱地址")
     if not re.match(r".+@.+\..+", email):
         return response_message.UserMessage.error("请输入正确的邮箱地址")
     code = gen_email_code()
     try:
-        send_email(email, code)
+        print("邮箱验证码: ", code)
         session["ecode"] = code.lower()
+        send_email(email, code)
         return response_message.UserMessage.success("验证码已发送")
     except Exception as e:
         print(e)
         return response_message.UserMessage.error("验证码发送失败")
+
+
+@user.route("/register", methods=["post"])
+def register():
+    request_data = json.loads(request.data)
+    username = request_data.get("username")
+    password = request_data.get("password")
+    second_password = request_data.get("second_password")
+    ecode = request_data.get("ecode")
+    print(
+        "username: {}, password: {}, second_password: {}, ecode: {}".format(
+            username, password, second_password, ecode
+        ),
+        "session ecode: {}".format(session.get("ecode")),
+    )
+    if ecode.lower() != session.get("ecode"):
+        return response_message.UserMessage.error("邮箱验证码错误")
+    if not re.match(r".+@.+\..+", username):
+        return response_message.UserMessage.error("请输入正确的邮箱地址")
+    if len(password) < 6:
+        return response_message.UserMessage.error("密码长度不能小于6位")
+    if password != second_password:
+        return response_message.UserMessage.error("两次输入的密码不一致")
+    user = User()
+    if len(user.find_by_username(username=username)) > 0:
+        return response_message.UserMessage.error("用户名已存在")
+
+    password = hashlib.md5(password.encode()).hexdigest()
+    result = user.do_register(username=username, password=password)
+
+    return response_message.UserMessage.success("注册成功")
--- model/user.py
+++ model/user.py
@@ -9,3 +9,21 @@ class User(Base):
 
     def get_one(self):
         return db_session.query(User).first()
+
+    def find_by_username(self, username):
+        return db_session.query(User).filter(User.username == username).all()
+
+    def do_register(self, username, password):
+        nickname = username.split("@")[0]
+        # picture_num = random.randint(1, 539)
+        job = "未定义"
+        user = User(
+            username=username,
+            password=password,
+            nickname=nickname,
+            picture="1.jpg",
+            job=job,
+        )
+        db_session.add(user)
+        db_session.commit()
+        return user
