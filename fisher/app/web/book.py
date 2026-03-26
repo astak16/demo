00a6cbd2ffa10548856048fd5533915c8ea2a02e
uccs <1500846601@qq.com>
2026-03-26 08:42:23 +0800

    /book/search 接口

--- fisher/app/web/book.py
+++ fisher/app/web/book.py
@@ -1,13 +1,16 @@
 from app.forms.book import SearchForm
+from app.view_models.book import BookCollection
 from helper import is_isbn_or_key
 from spider.yushu_book import YuShuBook
 from flask import jsonify, request
 from . import web
+import json
 
 
 @web.route("/book/search")
 def search():
     form = SearchForm(request.args)
+    book = BookCollection()
     if form.validate():
         q = form.q.data
         if q:
@@ -18,11 +21,14 @@ def search():
             page = 1
 
         isbn_or_key = is_isbn_or_key(q)
+        yushu_book = YuShuBook()
         if isbn_or_key == "isbn":
-            result = YuShuBook.search_by_isbn(q)
+            yushu_book.search_by_isbn(q)
         else:
-            result = YuShuBook.search_by_keyword(q, page)
+            yushu_book.search_by_keyword(q, page)
+        book.fill(yushu_book, q)
 
-        return jsonify(result)
+        # return jsonify(book)
+        return json.dumps(book, default=lambda o: o.__dict)
     else:
         return jsonify(form.errors)
--- fisher/spider/yushu_book.py
+++ fisher/spider/yushu_book.py
@@ -2,23 +2,35 @@ from flask import current_app
 
 
 class YuShuBook:
-    @classmethod
-    def search_by_isbn(cls, isbn):
-        return {
+    def __init__(self):
+        self.total = 0
+        self.books = []
+
+    def search_by_isbn(self, isbn):
+        result = {
             "total": 1,
             "books": [{"id": "9787501524044", "title": "Java 编程：从入门到实践"}],
         }
+        self.__fill_single(result)
 
-    @classmethod
-    def search_by_keyword(cls, keyword, page=1):
+    def __fill_single(self, data):
+        if data:
+            self.total = 1
+            self.books.append(data)
+
+    def __fill_collection(self, data):
+        self.total = data["total"]
+        self.books = data["books"]
+
+    def search_by_keyword(self, keyword, page=1):
         # per_page = current_app.config["PER_PAGE"]
-        cls.calculate_start(page)
-        return {
+        self.calculate_start(page)
+        result = {
             "total": 1,
             "books": [{"id": "9787501524044", "title": "Python 编程：从入门到实践"}],
         }
+        self.__fill_collection(result)
 
-    @staticmethod
-    def calculate_start(page):
+    def calculate_start(self, page):
         per_page = current_app.config["PER_PAGE"]
         return (page - 1) * per_page
