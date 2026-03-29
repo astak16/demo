7941540142b417971e477db202e7ae0c32b697ca
uccs <1500846601@qq.com>
2026-03-29 17:16:17 +0800

    异步发送邮件

--- fisher/app/libs/email.py
+++ fisher/app/libs/email.py
@@ -1,6 +1,15 @@
-from flask import current_app, render_template
+from threading import Thread
+from flask import current_app, render_template, copy_current_request_context
 from app import mail
 from flask_mail import Message
+
+
+def send_async_mail(app, msg):
+    with app.app_context():
+        try:
+            mail.send(msg)
+        except Exception as e:
+            ...
 
 
 def send_mail(to, subject, template, **kwargs):
@@ -10,4 +19,7 @@ def send_mail(to, subject, template, **kwargs):
         recipients=[to],
     )
     msg.html = render_template(template, **kwargs)
-    mail.send(msg)
+    thr = Thread(
+        target=copy_current_request_context(lambda: send_async_mail(current_app, msg))
+    )
+    thr.start()
